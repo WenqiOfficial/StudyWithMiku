1279599123d084f15f36d02586b5f039bdc4efe9
WenqiOfficial <2747297361@qq.com>
2023-08-07 14:55:16 +0800

    Update V1.0.3
    Add tips setting
    Update README&TODO

--- assets/js/mikuplayer.js
+++ assets/js/mikuplayer.js
@@ -1,6 +1,6 @@
 /* STUDY WITH MIKU
     CORE FUNCTION
-   V1.0.2 2023.08.05 */
+   V1.0.3 2023.08.07 */
 
    $(function() {
 	if (window.localStorage) {
@@ -17,17 +17,27 @@ var util = {
 		$(window).resize(util.videoresize);
 		$("#bt_fs").fadeIn(300, "linear");
 		$("#scene_top").fadeIn(300, "linear");
+		$(".aplayer-icon-lrc").trigger('click');
+		util.initTips("hitokoto");
+		util.initTips("studytime");
+		util.initTips("worldtime");
+		util.initClickEvent();
+		util.initStrictMode();
+		util.initWorldTimer();
+		util.readstoragetime()
+	},
+	initClickEvent: function() {
 		$("#bt_fs").on('click', function() {
 			util.fullscreen()
 		});
 		$("#btt_strict").on('click', function() {
-			util.switchStrictMode();
+			util.switchStrictMode()
 		});
 		$("#btt_start").on('click', function() {
 			if (util.checkStrictMode()) {
 				util.addVisibilityListener();
 			}
-			util.study();
+			util.study()
 		});
 		$("#btt_setting").on('click', function() {
 			util.menuopen("menu")
@@ -52,25 +62,7 @@ var util = {
 			$("#about_cover").fadeOut(300, "linear");
 			$("#scene_top").fadeIn(300, "linear")
 		});
-		util.initStrictMode();
-		util.initWorldTimer();
-		util.readstoragetime()
 	},
-	addVisibilityListener: function() {
-		document.addEventListener('visibilitychange', function() {
-			//fix wrong event's action
-			if (util.checkStrictMode() && recorded) {
-				if (document.visibilityState === 'hidden') {
-					$('#bt_rest').trigger('click');
-					document.title = '摸鱼中...';
-				}
-				if (document.visibilityState === 'visible') {
-					document.title = 'STUDY WITH MIKU';
-				}
-			}
-		});
-	},
-	check: function() {},
 	menuopen: function(e) {
 		$("#" + e).fadeIn(300, "linear");
 		$("#" + e + "_cover").fadeIn(300, "linear");
@@ -86,11 +78,11 @@ var util = {
 		$("#bt_rest").fadeIn(300, "linear");
 		$("video").trigger("play");
 		util.videoresize();
+		util.Tips.init();
 		util.timerecord.start();
-		util.tips.start();
 		$("#bt_stop").on('click', function() {
 			util.timerecord.stop();
-			util.tips.stop();
+			util.Tips.stop();
 			$('.aplayer-pause').trigger('click');
 			$("#scene_top").fadeIn(300, "linear");
 			$("#scene_learning").fadeOut(300, "linear");
@@ -99,6 +91,7 @@ var util = {
 			$("#rest_cover").fadeOut(300, "linear")
 		})
 	},
+	//Timer BEGIN
 	timerecord: {
 		start: function() {
 			clearInterval(time);
@@ -135,27 +128,28 @@ var util = {
 		wtime = setInterval(function() {
 			var myDate = new Date,
 				s = m = h = 0;
-				h=myDate.getHours();
-				m=myDate.getMinutes();
-				s=myDate.getSeconds();
-			if(myDate.getHours()<10){
-				h='0'+myDate.getHours()
+			h = myDate.getHours();
+			m = myDate.getMinutes();
+			s = myDate.getSeconds();
+			if (myDate.getHours() < 10) {
+				h = '0' + myDate.getHours()
 			}
-			if(myDate.getMinutes()<10){
-				m='0'+myDate.getMinutes()
+			if (myDate.getMinutes() < 10) {
+				m = '0' + myDate.getMinutes()
 			}
-			if(myDate.getSeconds()<10){
-				s='0'+myDate.getSeconds()
+			if (myDate.getSeconds() < 10) {
+				s = '0' + myDate.getSeconds()
 			}
 			$("#worldtime").text(h + "时" + m + "分" + s + "秒");
+			$("video").trigger("play")
 		}, 1000);
 	},
 	timer: function() {
-		var studytime = $("#time"),
-			tipstime = $("#tipstime"),
-			pastsDate = sDate = pastmDate = mDate = pasthDate = hDate = 0;
+		var pastsDate = sDate = pastmDate = mDate = pasthDate = hDate = 0;
 		time = setInterval(function() {
-			var myDate = new Date;
+			var studytime = $("#time"),
+				tipstime = $("#studytime"),
+				myDate = new Date;
 			sDate = myDate.getSeconds();
 			mDate = myDate.getMinutes();
 			hDate = myDate.getHours();
@@ -186,7 +180,6 @@ var util = {
 			pastsDate = sDate;
 			pastmDate = mDate;
 			pasthDate = hDate;
-			$("video").trigger("play")
 		}, 1000)
 	},
 	readstoragetime: function() {
@@ -210,84 +203,190 @@ var util = {
 		localStorage.setItem("studys", sumseconds.toString(16));
 		util.readstoragetime()
 	},
+	//Timer END
+	//StrictMode BEGIN
+	addVisibilityListener: function() {
+		document.addEventListener('visibilitychange', function() {
+			//fix wrong event's action
+			if (util.checkStrictMode() && recorded) {
+				if (document.visibilityState === 'hidden') {
+					$('#bt_rest').trigger('click');
+					document.title = '摸鱼中...'
+				}
+				if (document.visibilityState === 'visible') {
+					document.title = 'STUDY WITH MIKU'
+				}
+			}
+		});
+	},
 	checkStrictMode: function() {
 		let stat = localStorage.getItem("conf_strict") - '0';
 		//useful!
 		return stat
 	},
 	initStrictMode: function() {
-		if (util.checkStrictMode()==null) {
-			localStorage.setItem("conf_strict", 0);
-		}else if(util.checkStrictMode()){
-			$("#btt_strict")[0].innerText = '严格模式(离开页面自动停止): 开';
+		if (util.checkStrictMode() == null) {
+			localStorage.setItem("conf_strict", 0)
+		} else if (util.checkStrictMode()) {
+			$("#btt_strict")[0].innerText = '严格模式(离开页面自动停止): 开'
 		}
 	},
 	switchStrictMode: function() {
 		if (util.checkStrictMode()) {
 			$("#btt_strict")[0].innerText = '严格模式(离开页面自动停止): 关';
-			localStorage.setItem("conf_strict", 0);
+			localStorage.setItem("conf_strict", 0)
 		} else {
 			$("#btt_strict")[0].innerText = '严格模式(离开页面自动停止): 开';
 			localStorage.setItem("conf_strict", 1);
 		}
 	},
-	tips: {
+	//StrictMode END
+	//Tips BEGIN
+
+	//0:no display 1:roll display 2:always display
+	initTips: function(n) {
+		if (util.readTipsconf(n) == null || util.readTipsconf(n) == 1) {
+			localStorage.setItem("conf_tips_" + n, 1)
+		} else if (util.readTipsconf(n) == 2) {
+			localStorage.setItem("conf_tips_" + n, 2);
+			$("#" + n + "_mode").text("常驻")
+		} else {
+			localStorage.setItem("conf_tips_" + n, 0);
+			$("#" + n + "_mode").text("隐藏")
+		}
+		$("#btt_mode_" + n).on('click', function() {
+			util.switchTipsconf(n)
+		})
+	},
+	readTipsconf: function(n) {
+		let stat = localStorage.getItem("conf_tips_" + n) - '0';
+		return stat
+	},
+	//hitokoto,worldtime,studytime
+	switchTipsconf: function(n) {
+		if (util.readTipsconf(n) == null || util.readTipsconf(n) == 0) {
+			localStorage.setItem("conf_tips_" + n, 1);
+			$("#" + n + "_mode").text("轮换")
+		} else if (util.readTipsconf(n) == 1 && n != "hitokoto") {
+			localStorage.setItem("conf_tips_" + n, 2);
+			$("#" + n + "_mode").text("常驻")
+		} else {
+			localStorage.setItem("conf_tips_" + n, 0);
+			$("#" + n + "_mode").text("隐藏")
+		}
+	},
+	Tips: {
+		init: function() {
+			$(".tips").html('').prepend('<p class="attention">开始认真学习/工作吧！</p>').append('<p class="hitokoto" style="display: none;">"<hitokoto id="hitokoto">获取一言中...(*/ω＼*)</hitokoto>"</p>');
+			if (util.readTipsconf("worldtime") == 2) {
+				$(".attention").after('<p class="worldtime" style="display: none;">现在时间是:<text id="worldtime">0分钟</text>...</p>');
+				if (util.readTipsconf("studytime") == 2) {
+					$(".worldtime").after('<p class="studytime" style="display: none;">已经学习<text id="studytime">0分钟</text>了呢...</p>')
+				} else {
+					$(".tips").append('<p class="studytime" style="display: none;">已经学习<text id="studytime">0分钟</text>了呢...</p>')
+				}
+			} else {
+				if (util.readTipsconf("studytime") == 2) {
+					$(".attention").after('<p class="studytime" style="display: none;">已经学习<text id="studytime">0分钟</text>了呢...</p>')
+				} else {
+					$(".tips").append('<p class="studytime" style="display: none;">已经学习<text id="studytime">0分钟</text>了呢...</p>')
+				}
+				$(".tips").append('<p class="worldtime" style="display: none;">现在时间是:<text id="worldtime">0分钟</text>...</p>')
+			}
+			util.Tips.start()
+		},
 		start: function() {
-			tips = setInterval(function() {
-				//1:hitokoto;2:ntime;3:worldtime
-				if (tipstype == 0 || tipstype == 3) {
-					tipstype = 1;
+			attentionout = setTimeout(function() {
+				$(".attention").fadeOut(200, "linear");
+				if (util.readTipsconf("hitokoto") == 1) {
+					tipsrollnow = "hitokoto"
+				} else if (util.readTipsconf("worldtime") == 1) {
+					tipsrollnow = "worldtime"
+				} else if (util.readTipsconf("studytime") == 1) {
+					tipsrollnow = "studytime"
+				}
+				if (util.readTipsconf("worldtime") == 2) {
+					$(".worldtime").fadeIn(300, "linear")
+				}
+				if (util.readTipsconf("studytime") == 2) {
+					$(".studytime").fadeIn(300, "linear")
+				}
+				util.Tips.roll();
+			}, 6000)
+		},
+		roll: function() {
+			if (util.readTipsconf(tipsrollnow) == 1) {
+				if (tipsrollnow == "hitokoto") {
+					if (util.readTipsconf("worldtime") == 1) {
+						tipsrollnow = "worldtime"
+					} else if (util.readTipsconf("studytime") == 1) {
+						tipsrollnow = "studytime"
+					}
 					async function fetchHitokoto() {
+						const hitokoto = document.querySelector('#hitokoto');
+						hitokoto.innerText = "获取一言中...(*/ω＼*)";
 						const response = await fetch('https://v1.hitokoto.cn/?c=d&c=i&c=k&max_length=10');
 						const {
 							uuid, hitokoto: hitokotoText
 						} = await response.json();
-						const hitokoto = document.querySelector('#hitokoto');
 						hitokoto.innerText = hitokotoText
 					}
 					fetchHitokoto();
-					sayingin = setTimeout(function() {
-						$(".saying").fadeIn(300, "linear")
-					}, 1000);
-					sayingout = setTimeout(function() {
-						$(".saying").fadeOut(300, "linear")
-					}, 14300)
-				} else if (tipstype == 1) {
-					tipstype = 2;
-					tipstimein = setTimeout(function() {
-						$(".ntime").fadeIn(300, "linear")
-					}, 1000);
-					tipstimeout = setTimeout(function() {
-						$(".ntime").fadeOut(300, "linear")
-					}, 14300)
+					hitokotoin = setTimeout(function() {
+						$(".hitokoto").fadeIn(300, "linear")
+					}, 1300);
+					hitokotoout = setTimeout(function() {
+						$(".hitokoto").fadeOut(300, "linear")
+					}, 8600)
+				} else if (tipsrollnow == "worldtime") {
+					if (util.readTipsconf("studytime") == 1) {
+						tipsrollnow = "studytime"
+					} else if (util.readTipsconf("hitokoto") == 1) {
+						tipsrollnow = "hitokoto"
+					}
+					worldtimein = setTimeout(function() {
+						$(".worldtime").fadeIn(300, "linear")
+					}, 1300);
+					worldtimeout = setTimeout(function() {
+						$(".worldtime").fadeOut(300, "linear")
+					}, 8600)
 				} else {
-					tipstype = 3;
-					tipstimein = setTimeout(function() {
-						$(".wtime").fadeIn(300, "linear")
-					}, 1000);
-					tipstimeout = setTimeout(function() {
-						$(".wtime").fadeOut(300, "linear")
-					}, 14300)
+					if (util.readTipsconf("hitokoto") == 1) {
+						tipsrollnow = "hitokoto"
+					} else if (util.readTipsconf("worldtime") == 1) {
+						tipsrollnow = "worldtime"
+					}
+					studytimein = setTimeout(function() {
+						$(".studytime").fadeIn(300, "linear")
+					}, 1300);
+					studytimeout = setTimeout(function() {
+						$(".studytime").fadeOut(300, "linear")
+					}, 8600)
 				}
-			}, 14600);
-			attentionout = setTimeout(function() {
-				$(".attention").fadeOut(300, "linear")
-			}, 8000)
+				rolltimeout = setTimeout(function() {
+					util.Tips.roll()
+				}, 10900)
+			}
 		},
 		stop: function() {
-			clearTimeout(tips);
-			clearTimeout(sayingin);
-			clearTimeout(sayingout);
-			clearTimeout(tipstimein);
-			clearTimeout(tipstimeout);
+			clearTimeout(rolltimeout);
+			clearTimeout(hitokotoin);
+			clearTimeout(hitokotoout);
+			clearTimeout(worldtimein);
+			clearTimeout(worldtimeout);
+			clearTimeout(studytimein);
+			clearTimeout(studytimeout);
 			clearInterval(attentionout);
-			$(".saying").fadeOut(300, "linear");
-			$(".ntime").fadeOut(300, "linear");
+			$(".studytime").fadeOut(300, "linear");
+			$(".hitokoto").fadeOut(300, "linear");
+			$(".worldtime").fadeOut(300, "linear");
 			setTimeout(function() {
 				$(".attention").fadeIn(300, "linear")
 			}, 300)
 		}
 	},
+
+	//Tips END
 	videoresize: function() {
 		var ww = $(window).width(),
 			wh = $(window).height(),
@@ -305,5 +404,5 @@ var util = {
 	checkFullscreen: function() {
 		return !!(document.webkitFullscreenElement || document.mozFullScreenElement || document.mozFullScreenElement || document.msFullscreenElement || document.fullscreenElement)
 	}
-}, hour = minutes = seconds = recorded = sumhour = summinutes = sumseconds = tipstype = tipstimein = tipstimeout = sayingin = sayingout = attentionout = 0;
-console.log("\n %c Study With Miku V1.0.2 %c 在干什么呢(・∀・(・∀・(・∀・*) \n", "color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0; color: #000")
+}, hour = minutes = seconds = recorded = sumhour = summinutes = sumseconds = tipstype = rolltimeout = worldtimein = worldtimeout = studytimein = studytimeout = hitokotoin = hitokotoout = attentionout = tipsrollnow = 0;
+console.log("\n %c Study With Miku V1.0.3 %c 在干什么呢(・∀・(・∀・(・∀・*) \n", "color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0; color: #000")
