063764a9aa53c6c4274a2a5dc6ccf8f7dca937cb
WenqiOfficial <2747297361@qq.com>
2023-09-01 02:06:50 +0800

    dev sw

--- sw.dev.js
+++ sw.dev.js
@@ -1,31 +1,64 @@
-var cacheStorageKey = 'minimal-pwa-1'
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = "offline";
 
-var cacheList = [
-  '/',
-  "index.html",
-  "assets/css/main.css",
-  "https://cdn.bootcdn.net/ajax/libs/jquery/3.6.4/jquery.min.js",
-  "https://cdn.bootcdn.net/ajax/libs/aplayer/1.10.1/APlayer.min.js",
-  "https://cdn.bootcdn.net/ajax/libs/meting/2.0.1/Meting.min.js",
-  "https://cdn.bootcdn.net/ajax/libs/countup.js/1.9.3/countUp.min.js",
-  "assets/css/shared/sp/css/common.css",
-  "https://cdn.bootcdn.net/ajax/libs/aplayer/1.10.1/APlayer.min.css",
-  "assets/js/miku.js",
-  "assets/video/oneloop.mp4"
-]
-
-self.addEventListener('activate', function(e) {
-    e.waitUntil(
-      Promise.all(
-        caches.keys().then(cacheNames => {
-          return cacheNames.map(name => {
-            if (name !== cacheStorageKey) {
-              return caches.delete(name)
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      (async () => {
+        const cache = await caches.open(CACHE_NAME);
+        // Setting {cache: 'reload'} in the new request will ensure that the
+        // response isn't fulfilled from the HTTP cache; i.e., it will be from
+        // the network.
+        await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+      })()
+    );
+    // Force the waiting service worker to become the active service worker.
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", (event) => {
+    event.waitUntil(
+      (async () => {
+        // Enable navigation preload if it's supported.
+        // See https://developers.google.com/web/updates/2017/02/navigation-preload
+        if ("navigationPreload" in self.registration) {
+          await self.registration.navigationPreload.enable();
+        }
+      })()
+    );
+  
+    // Tell the active service worker to take control of the page immediately.
+    self.clients.claim();
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    // We only want to call event.respondWith() if this is a navigation request
+    // for an HTML page.
+    if (event.request.mode === "navigate") {
+      event.respondWith(
+        (async () => {
+          try {
+            // First, try to use the navigation preload response if it's supported.
+            const preloadResponse = await event.preloadResponse;
+            if (preloadResponse) {
+              return preloadResponse;
             }
-          })
-        })
-      ).then(() => {
-        return self.clients.claim()
-      })
-    )
-  })
+  
+            // Always try the network first.
+            const networkResponse = await fetch(event.request);
+            return networkResponse;
+          } catch (error) {
+            // catch is only triggered if an exception is thrown, which is likely
+            // due to a network error.
+            // If fetch() returns a valid HTTP response with a response code in
+            // the 4xx or 5xx range, the catch() will NOT be called.
+            console.log("Fetch failed; returning offline page instead.", error);
+  
+            const cache = await caches.open(CACHE_NAME);
+            const cachedResponse = await cache.match(OFFLINE_URL);
+            return cachedResponse;
+          }
+        })()
+      );
+    }
+  
+  });
