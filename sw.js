a50398b5ea55c227366b858de24e1bcec751131d
WenqiOfficial <2747297361@qq.com>
2024-07-03 14:11:44 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -44,6 +44,7 @@ this.addEventListener('fetch', function (event) {
           function (response) {
             // 检测返回数据是否有效
             console.log("eeee: "+fetchRequest.headers)
+            console.log("res: "+response)
             if (!response || response.status !== 200 || response.type !== 'basic' || fetchRequest.headers.get("accept").indexOf("video/mp4") !== -1) {
               return response;
             }
